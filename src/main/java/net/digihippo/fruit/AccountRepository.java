43facb1273717a1d40ad7f22e675e799e65c96d6
James Byatt <grumpyjames@gmail.com>
2014-04-24 11:26:56 +0100

    extract an account repository to look after accounts

---
+++ src/main/java/net/digihippo/fruit/AccountRepository.java
@@ -0,0 +1,19 @@
+package net.digihippo.fruit;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountRepository {
+    final Map<Integer, Account> accounts = new HashMap<Integer, Account>();
+
+    public AccountRepository() {
+    }
+
+    void addAccount(int id, Account newAccount) {
+        accounts.put(id, newAccount);
+    }
+
+    Account getAccount(int accountId) {
+        return accounts.get(accountId);
+    }
+}
--- src/main/java/net/digihippo/fruit/BreadShop.java
+++ src/main/java/net/digihippo/fruit/BreadShop.java
@@ -1,25 +1,23 @@
 package net.digihippo.fruit;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class BreadShop {
     public static int PRICE_OF_BREAD = 12;
 
     private final OutboundEvents events;
-    private final Map<Integer, Account> accounts = new HashMap<Integer, Account>();
+    private final AccountRepository accountRepository = new AccountRepository();
 
     public BreadShop(OutboundEvents events) {
         this.events = events;
     }
 
     public void createAccount(int id, String forename, String surname) {
-        accounts.put(id, new Account(id, forename, surname));
+        Account newAccount = new Account(id, forename, surname);
+        accountRepository.addAccount(id, newAccount);
         events.accountCreatedSuccessfully(id);
     }
 
     public void deposit(int accountId, int creditAmount) {
-        Account account = accounts.get(accountId);
+        Account account = accountRepository.getAccount(accountId);
         if (account != null) {
             final int newBalance = account.deposit(creditAmount);
             events.newAccountBalance(accountId, newBalance);
@@ -29,7 +27,7 @@ public class BreadShop {
     }
 
     public void placeOrder(int accountId, int orderId, int amount) {
-        Account account = accounts.get(accountId);
+        Account account = accountRepository.getAccount(accountId);
         if (account != null) {
             int cost = amount * PRICE_OF_BREAD;
             if (account.getBalance() > cost) {
@@ -46,7 +44,7 @@ public class BreadShop {
     }
 
     public void cancelOrder(int accountId, int orderId) {
-        Account account = accounts.get(accountId);
+        Account account = accountRepository.getAccount(accountId);
         if (account == null)
         {
             events.accountNotFound(accountId);
