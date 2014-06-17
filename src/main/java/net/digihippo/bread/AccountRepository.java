67aea8a98445921a625dee2c2af7a22035ea7b5c
James Byatt <grumpyjames@gmail.com>
2014-06-17 20:22:37 +0100

    make erroneously visible field a little more hidden

--- src/main/java/net/digihippo/bread/AccountRepository.java
+++ src/main/java/net/digihippo/bread/AccountRepository.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AccountRepository {
-    final Map<Integer, Account> accounts = new HashMap<Integer, Account>();
+    private final Map<Integer, Account> accounts = new HashMap<Integer, Account>();
 
     public AccountRepository() {
     }
